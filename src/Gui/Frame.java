af93b3b5c763902f5fd96566aa65225f87638791
Renana <renanalevi43@gmail.com>
2020-12-11 15:32:59 +0200

    mid

--- src/Gui/Frame.java
+++ src/Gui/Frame.java
@@ -1,4 +1,27 @@
-//package gameClient;
+package Gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Frame extends JFrame {
+
+    public Frame(){
+        super();
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenHeight = screenSize.height;
+        int screenWidth = screenSize.width;
+
+        this.setTitle("Catch The Pokemon"); //sets title of frame
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit from application
+        this.setResizable(false); //prevent frame from being resized
+        this.setSize(screenWidth,screenHeight); //sets the x-dimension, and y-dimension of frame
+        this.setVisible(true); //make frame visible
+        ImageIcon image = new ImageIcon("./resources/Icon.png"); //create an ImageIcon
+        this.setIconImage(image.getImage()); //change icon of frame
+//        this.getContentPane().setBackground(new Color(0,122 ,232)); //change color of background
+    }
+}
 //
 //import api.directed_weighted_graph;
 //import api.edge_data;
